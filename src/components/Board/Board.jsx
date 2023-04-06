e86d4b0c1ca1e1c6c9723fce456a365e5fe3e2f5
Zain Kazi <zainkazi27@gmail.com>
2023-04-06 10:28:47 +0500

    rendered one piece on board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Piece from "../Piece/Piece";
 import Square from "../Square/Square";
 import "./Board.css";
 
 //Pieces
-import kingBlack from "../../assets/pieces/king-black.svg";
+import kingWhite from "../../assets/pieces/white-king.svg";
+import kingBlack from "../../assets/pieces/black-king.svg";
+import queenWhite from "../../assets/pieces/white-queen.svg";
+import queenBlack from "../../assets/pieces/black-queen.svg";
+import rookWhite from "../../assets/pieces/white-rook.svg";
+import rookBlack from "../../assets/pieces/black-rook.svg";
+import bishopWhite from "../../assets/pieces/white-bishop.svg";
+import bishopBlack from "../../assets/pieces/black-bishop.svg";
+import knightWhite from "../../assets/pieces/white-knight.svg";
+import knightBlack from "../../assets/pieces/black-knight.svg";
+import whitePawn from "../../assets/pieces/white-pawn.svg";
+import blackPawn from "../../assets/pieces/black-pawn.svg";
 
 let temp = [];
-let board = [];
+let initialBoard = [];
+let lookupTable = {};
 
 for (var i = 1; i <= 8; i++) {
   for (var j = 1; j <= 8; j++) {
     temp.push({
       id: { i, j },
-      name: `${String.fromCharCode(96 + i)}${j}`,
+      name: `${String.fromCharCode(96 + j)}${i}`,
       color: (i + j) % 2 == 0 ? "bg-gray-600" : "bg-white",
       occupiedBy: "",
     });
+
+    // Create the Lookup table
+    // if (`${String.fromCharCode(96 + i)}${j}`[1] == "2") {
+    //   lookupTable[`${String.fromCharCode(96 + i)}${j}`] = whitePawn;
+    // } else if (`${String.fromCharCode(96 + i)}${j}`[1] == "7") {
+    //   lookupTable[`${String.fromCharCode(96 + i)}${j}`] = blackPawn;
+    // } else {
+    //   lookupTable[`${String.fromCharCode(96 + i)}${j}`] = "";
+    // }
+
+    lookupTable[`${String.fromCharCode(96 + i)}${j}`] = { name: "", url: "" };
+    lookupTable["e1"] = {
+      name: "king-white",
+      imageUrl: kingWhite,
+    };
   }
 }
 
 for (var i = 63; i >= 0; i -= 8) {
-  board.push(temp.slice(i - 7, i + 1));
+  initialBoard.push(temp.slice(i - 7, i + 1));
 }
-board = [].concat(...board);
+initialBoard = [].concat(...initialBoard);
 
 const Board = () => {
-  console.log(board);
+  const [board, setBoard] = useState(initialBoard);
+
+  console.log(lookupTable);
 
   return (
     <div className="board">
-      {board.map((square) => (
+      {initialBoard.map((square) => (
         <Square
           key={square.name}
           id={square.id}
@@ -38,7 +67,9 @@ const Board = () => {
           color={square.color}
           occupiedBy={square.occupiedBy}
         >
-          {square.occupiedBy.length == 0 ? null : <Piece image={kingBlack} />}
+          {lookupTable[square.name].name.length == 0 ? null : (
+            <Piece image={lookupTable[square.name].imageUrl} />
+          )}
         </Square>
       ))}
     </div>
